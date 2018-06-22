42bff1f6048ba681460499359a062be481574d4b
Ogundiran Ayobami <ayobami_ogundiran@yahoo.com>
2018-06-22 02:25:18 -0700

    Update to remove share button

--- resources/views/pages/result.blade.php
+++ resources/views/pages/result.blade.php
@@ -14,9 +14,6 @@
 	  <a class="btn btn-danger" href=" {{ $image }}" download>Click to download</a>
 	  <br><br>
 	  <div class="fb-like" data-href="https://www.facebook.com/motutordotcom" data-layout="standard" data-action="like" data-size="large" data-show-faces="false" data-share="false"></div>
-
-     <div class="fb-share-button" data-href="https://eventimage.herokuapp.com" data-layout="button_count" data-size="large" data-mobile-iframe="true"><a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Feventimage.herokuapp.com%2F&amp;src=sdkpreparse" class="fb-xfbml-parse-ignore">Share</a></div>
-     
     <br>Click like to appreciate our efforts.
 	</div>
    @endsection
